515b726f0e115d63f17c1802089b344459d7f305
Juferoga <juancholoco2000.61@gmail.com>
2019-08-27 11:23:40 -0500

    Version 2.0

---
+++ Bash/SistemasOperativos/FormatoCarpetas2.sh
@@ -0,0 +1,5 @@
+#! /bin/bash 
+
+mkdir -p JuanFelipe/{a/e/{j,k/o/{r,s},l},b/{f,g},c/h/m/p/{x,y/{AA,AB,AC}},d/i/n/q/z/{AD,AE/AF/AG}}
+
+# Version 2.0 una sola linea
--- Bash/SistemasOperativos/formatoCarpetas.sh
+++ Bash/SistemasOperativos/formatoCarpetas.sh
@@ -20,6 +20,21 @@ mkdir c/h/m/p/x
 mkdir -p d/i/n/q/z/AE/AF/AG
 mkdir -p d/i/n/q/z/AD
 
-tree
+touch 1.txt 2.txt 3.txt 4.txt 5.txt  a.txt
+
+echo "Archivos creados"
+ls
+
+cp *.txt /d/i/n/q/z/
+echo "Archivos Copiados"
+ls /d/i/n/q/z/
+
+#Version 1.0 creacion en diferentes lineas
+
+
+
+
+
+
 
 
