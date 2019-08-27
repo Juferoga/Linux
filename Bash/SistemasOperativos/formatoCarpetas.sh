2bbaa0ba39c5401c04bac7dee4499166d1ada978
Juan Felipe R <42847785+Juferoga@users.noreply.github.com>
2019-08-27 10:48:22 -0500

    Create formatoCarpetas.sh

---
+++ Bash/SistemasOperativos/formatoCarpetas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir juanfelipe
+
+cd juanfelipe
+
+mkdir -p a/e/k/o/r
+mkdir a/e/j
+mkdir a/e/l
+mkdir a/e/k/o/y
+
+mkdir -p b/g
+mkdir b/f
+
+mkdir -p c/h/m/p/y/AA
+mkdir c/h/m/p/y/AB
+mkdir c/h/m/p/y/AC
+mkdir c/h/m/p/x
+
+mkdir -p d/i/n/q/z/AE/AF/AG
+mkdir -p d/i/n/q/z/AD
+
+tree
+
+
