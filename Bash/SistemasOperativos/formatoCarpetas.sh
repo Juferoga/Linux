907db8508413c612ff4c9ac3a17263b16b6beab9
Juferoga <juancholoco2000.61@gmail.com>
2019-08-29 11:01:17 -0500

    DOS :(

--- Bash/SistemasOperativos/formatoCarpetas.sh
+++ Bash/SistemasOperativos/formatoCarpetas.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
 mkdir juanfelipe
+# md juanfelipe
 
 cd juanfelipe
+#dir juanfelipe
 
 mkdir -p a/e/k/o/r
 mkdir a/e/j
 mkdir a/e/l
 mkdir a/e/k/o/y
 
+#DOS
+# md a/e/k/o
+# md a/e/j
+# md a/e/l
+# md a/e/k/o/y
+
 mkdir -p b/g
 mkdir b/f
+
+# md b/g
+# md b/f
 
 mkdir -p c/h/m/p/y/AA
 mkdir c/h/m/p/y/AB
 mkdir c/h/m/p/y/AC
 mkdir c/h/m/p/x
 
+# md c/h/m/p/y/AA
+# md c/h/m/p/y/AB
+# md c/h/m/p/y/AC
+# md c/h/m/p/x
+
 mkdir -p d/i/n/q/z/AE/AF/AG
 mkdir -p d/i/n/q/z/AD
+
+# md d/i/n/q/z/AE/AF/AG
+# md d/i/n/q/z/AD
 
 touch 1.txt 2.txt 3.txt 4.txt 5.txt  a.txt
 
